d7bfa53532c30eb6c60f75cba3ecf7ba1dd75d00
carotkut94 <carotkut12@gmail.com>
2023-09-18 18:35:53 +0530

    Initial Commit

---
+++ ClipStack/ClipStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipStackApp.swift
+//  ClipStack
+//
+//  Created by Deathcode on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ClipStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
